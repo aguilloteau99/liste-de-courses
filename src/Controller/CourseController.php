aa3d08551042ed1ff1cd0004426c765371db668a
aguilloteau99 <alexandreguilloteau99@gmail.com>
2021-09-22 16:21:10 +0200

    all good

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -2,19 +2,58 @@
 
 namespace App\Controller;
 
+use App\Entity\ListeEntity;
+use App\Form\CourseFormType;
+use App\Repository\ListeEntityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class CourseController extends AbstractController
 {
     /**
-     * @Route("/course", name="course")
+     * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(ListeEntityRepository $repo, Request $request): Response
     {
+        $courses = $repo->findAll();
+        $newCourses = new ListeEntity();
+        $newCourses->setIsBuy(false);
+        $formCourse = $this->createForm(CourseFormType::class, $newCourses);
+        $formCourse->handleRequest($request);
+        if ($formCourse->isSubmitted()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($newCourses);
+            $em->flush();
+            return $this->redirectToRoute('index');
+        }
         return $this->render('course/index.html.twig', [
-            'controller_name' => 'CourseController',
+            'courses' => $courses,
+            'formCourse'=> $formCourse->createView()
         ]);
+    }
+
+    /**
+     * @Route("/buy/{id}", name="buy")
+     */
+    public function buy(ListeEntity $list, ListeEntityRepository $repo, Request $request): Response
+    {
+        $list->setIsBuy(!$list->getIsBuy());
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($list);
+        $em->flush();
+        return $this->index($repo, $request);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete(ListeEntity $list, ListeEntityRepository $repo, Request $request): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($list);
+        $em->flush();
+        return $this->index($repo, $request);
     }
 }
---
+++ src/Entity/ListeEntity.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ListeEntityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ListeEntityRepository::class)
+ */
+class ListeEntity
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isBuy;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getIsBuy(): ?bool
+    {
+        return $this->isBuy;
+    }
+
+    public function setIsBuy(bool $isBuy): self
+    {
+        $this->isBuy = $isBuy;
+
+        return $this;
+    }
+}
